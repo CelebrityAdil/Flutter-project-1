2171e7d9fd77d67b4380b2085f5aaa912c7a6431
Adil Qayyum <adilqayyumkhan@gmail.com>
2025-10-04 09:42:17 -0700

    updated code

--- lib/rowoperations.dart
+++ lib/rowoperations.dart
@@ -14,7 +14,7 @@ class Rowoperations extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(width: 50, height: 50, color: Colors.red),
+                Container(width: 50, height: 60, color: Colors.red),
                 Container(
                   width: 50,
                   height: 50,
@@ -31,10 +31,18 @@ class Rowoperations extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(width: 60, height: 60, color: Colors.green),
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 61, 128, 64),
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                  ),
+                ),
                 Container(width: 60, height: 60, color: Colors.orange),
               ],
             ),
+            SizedBox(height: 20), // Space between rows
           ],
         ),
       ),
